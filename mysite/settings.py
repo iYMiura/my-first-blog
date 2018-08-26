74ed2349f2256c527df5648c3d0c3eeb47d3bf72
iYMiura <stad.miura@gmail.com>
2018-08-26 22:49:32 +0900

    Added more views to the website.

--- mysite/settings.py
+++ mysite/settings.py
@@ -115,3 +115,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# tutorial add
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
